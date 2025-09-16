bb7a3fab8bfdd126e127a07850eff7617ea7d72c
mehdi zouine <mehdi07zouine@gmail.com>
2025-09-16 23:11:03 +0100

    oo

--- admin.php
+++ admin.php
@@ -174,7 +174,7 @@ $conn->close();
     <div class="container">
       <div class="copyright-box">
         <p class="copyright">&copy; Copyright <strong>[school's name]</strong> 2025-2026</p>
-        <div class="credits">All Rights Resd</div>
+        <div class="credits">All Rights Resdoo</div>
       </div>
     </div>      
   </footer>
